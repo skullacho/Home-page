b62090f01095edad35530e36532dbfdb95c3b79d
skullacho <pedrosmbsilva@gmail.com>
2026-04-08 21:57:49 -0300

    Add files via upload

--- Homepage/style.js
+++ Homepage/style.js
@@ -1,3 +1,38 @@
+
+function setAba(tipo) {
+  const btnCompreja = document.getElementById('btn-buscar-main');
+        
+ }
+
+// Função para o botão de busca
+function buscar() {
+  const local = document.getElementById('select-local').value;
+    if (local === "") {
+      alert("Por favor, selecione uma localização!");
+    } else {
+    alert("Buscando imóveis em: " + local);
+    }
+  }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tab-btn");
+
+  tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+
+      // remove active de todos
+      tabs.forEach(t => t.classList.remove("active"));
+
+      // adiciona no clicado
+      tab.classList.add("active");
+
+    });
+  });
+});
+function toggleMenu() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
+
 var map = L.map('map').setView([-23.55, -46.63], 11); // São Paulo
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
